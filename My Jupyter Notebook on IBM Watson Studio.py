9c763979fbc2076040d6d2450b923a39283bf60b
Latman14 <114597501+Latman14@users.noreply.github.com>
2022-10-03 15:11:26 +0100

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# "My Jupyter Notebook on IBM Watson Studio"
+
+# "ADEPOJU ABDULLATEEF ADEWALE"
+
+# "Student"
+
+# "I am interested in data science because, i love data and want to be a data scientist and data analyst in the nearest future".
+
+# "Creating a BMI calulator and giving output"
+
+# In[ ]:
+
+
+print('welcome to your BMI calculator!!!')
+
+weight=int(input('Enter your weight in Kg: '))
+
+height=float(input('Enter your height in M: '))
+
+answer=print(weight/(height*height))
+
+
+# In[ ]:
+
+
+
+
